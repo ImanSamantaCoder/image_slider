0d89289d73aeabcef9a6a3f851fb50927c2d472d
ImanSamantaCoder <iman.samanta.code@gmail.com>
2025-03-07 01:53:46 +0530

    Updated code with changes

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,22 +1,38 @@
-import React,{useState} from "react"
+import React,{useState,useRef,useEffect} from "react"
 import data from "./data.json"
 const Slider = ()=>{
     console.log(data[0].download_url)
     const [next,setnext] = useState(0);
+    let ref = useRef(null);
+    
     const handleNext = ()=>{
-        if(next==data.length-1)
+        setnext((previousValue)=>{
+            if(previousValue==data.length-1)
+            {
+                return 0;
+            }
+            return previousValue+1;
+        })
+    }
+    const handlePre = ()=>{
+        if(next==0)
         {
-            setnext(0)
+            setnext(data.length-1)
         }
-        else
-        {
-        setnext(next+1);
+        else{
+            setnext(next-1);
         }
     }
+    useEffect(()=>{
+      ref.current = setInterval(handleNext,1000);
+      return(()=>{
+         clearInterval(ref.current)
+      })
+    },[])
     return (
-        <div className="container">
+        <div className="container" onMouseEnter={()=>{clearInterval(ref.current)}} onMouseLeave={()=>ref.current = setInterval(handleNext,1000)}>
             <div className="left-btn">
-                 {/* <button onClick={handlePrevious}>{"<"}</button> */}
+                 <button onClick={handlePre}>{"<"}</button>
             </div>
             <img src={data[next].download_url} alt="image"/>
             <div className="right-btn">
